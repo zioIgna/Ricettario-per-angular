327cd925ee93bba4171fb5089c93ac11593ea33c
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-12 16:30:21 +0200

    3.55

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
