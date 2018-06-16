c50802e0951a273f656572471beec9d10a8b6e91
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-16 12:44:36 +0200

    Mie modifiche non funzionante

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes',10),
-  ];
 
-  constructor() { }
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
   onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+    this.shoppingListService.onIngredientAdded(ingredient);
   }
-
 }
